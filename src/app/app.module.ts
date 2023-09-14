a34513888613ee085f356da01d14954b2e59599a
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2023-09-14 11:04:09 +0530

    Inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
